413deca8be2d563939104c64d3494f186607090c
kmhosny <kmhosny@yahoo.com>
2016-09-17 21:28:49 +0200

    added the the concurrency connections to Redis db

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # go-pool-manager
+- This project uses the Radix.v2 library for Redis connection available at https://github.com/mediocregopher/radix.v2
+as recommended by Redis itself http://redis.io/clients#go
+
+- it makes use of Pool object which is the thread safe connection pool of the project.
+- it works by launching total_processors go rountines into the wild and let them do their own logic in retrieving an event ID and assign it to the shell script. if the id returned is 0 which is converted to -1 it means the list doesn't contain any IDs and the job is done.
+
+- the code will look for  a Redis list names testlist assuming it contains the list of IDs it needs.
--- main.go
+++ main.go
@@ -1,42 +1,125 @@
-package main
+//i changed the idea behind the jobs scheduler, instead of waiting for a main routine to assign the ID for the "job/subroutine"
+//the main go routine will launch total_processors go rountines into the wild and let them do their own logic in retrieving
+//an event ID and assign it to the shell script.
+//if the id returned is 0 which is converted to -1 it means the list doesn't contain any IDs and the job is done.
 
+package main
 import (
+  "github.com/mediocregopher/radix.v2/pool"
   "fmt"
+  "sync"
+  "time"
   "strconv"
   "os/exec"
-  )
+)
 
-func worker(id int, jobs <-chan int, results chan<- int) {
-    for j := range jobs {
-        fmt.Println("worker", id, "processing job", j)
-        out, err := exec.Command("./foo.sh", strconv.Itoa(j)).Output()
-        if err != nil {
-          // TODO: capture this in a log file
-          // log.Fatal(err)
-        }
-        fmt.Printf("= %s\n", out)
-        results <- j * 2
-    }
+const listname = "testlist"
+const total_processors = 15
+
+//logging success and failure per routine
+type Log struct{
+  success []string
+  fail []string
 }
 
+func (l *Log) printSuccess(){
+  fmt.Println(l.success)
+}
+
+func (l *Log) printFail(){
+  fmt.Println(l.fail)
+}
+
+
+//struct to hold the connection pool - as recommended by the library for concurrency purposes-
+//and wait group that will signal if all the routines are done.
+type EventsCon struct{
+  p *pool.Pool
+  wg sync.WaitGroup
+}
+
+//creating a connection to redis
+func (e *EventsCon) createNewConnection(){//this connection will be shared with all running threads
+  var err error
+  e.p, err = pool.New("tcp", "localhost:6379", 10)
+  if err!=nil{
+    fmt.Println("error construction a connection")
+    return
+  }
+}
+
+// retrieving next ID, this function makes sure to have a thread safe connection to redis
+//since it uses pool.Cmd - based on radix.v2 documentation
+func (e *EventsCon) getNextId() (int,error){
+
+
+  response,err := e.p.Cmd("LPOP",listname).Int()
+  if err!=nil{
+    //fmt.Println("ERROR in RESPONSE")
+    return -1,err
+  }
+
+  return response,nil
+}
+
+
+//the go routine that will be launched, it gets the EventsCon pointer and a jobLog pointer and it's own ID
+//if the id of the event is -1 then no more events in the Redis list and it can call wg.Done() to signal
+//that it's over.
+func processJob(e *EventsCon, jobLog *Log, myId int){
+  id,err:=e.getNextId()
+  if err!=nil{
+    if id==-1 {
+      jobLog.success = append(jobLog.success,"no more IDs")
+      fmt.Println("Routine "+strconv.Itoa(myId)+" says no more IDs")
+    }else{
+      jobLog.fail = append(jobLog.fail,"error getting the ID: "+err.Error()+" returned ID="+strconv.Itoa(id))
+    }
+  }
+
+  for id!=-1 {
+
+    fmt.Println("response is "+ strconv.Itoa(id) + " for rountine " +strconv.Itoa(myId))
+    start:=time.Now()
+    out, err := exec.Command("./foo.sh", strconv.Itoa(id)).Output()
+    if err != nil {
+      // TODO: capture this in a log file
+      // log.Fatal(err)
+      jobLog.fail = append(jobLog.fail, "event "+strconv.Itoa(id)+" has failed")
+    } else {
+      fmt.Printf("= %s\n", out)
+      elapsed := time.Since(start)
+      jobLog.success = append(jobLog.success, "event "+strconv.Itoa(id)+" has succeded in "+elapsed.String())
+    }
+    id,err=e.getNextId()
+    if err!=nil{
+      if id==-1 {
+        jobLog.success = append(jobLog.success,"no more IDs")
+        fmt.Println("Routine "+strconv.Itoa(myId)+" says no more IDs")
+      }else{
+        jobLog.fail = append(jobLog.fail,"error getting the ID: "+err.Error()+" returned ID="+strconv.Itoa(id))
+      }
+    }
+  }
+  e.wg.Done()
+}
+
+//launch total_processors go rountines then wait for all done signal close the connection
+//then print the log
 func main() {
-    concurrency := 8
-    total := 12
-
-    jobs := make(chan int, 100)
-    results := make(chan int, 100)
-
-    for w := 1; w <= concurrency; w++ {
-        go worker(w, jobs, results)
-    }
-
-    for j := 1; j <= total; j++ {
-        jobs <- j
-    }
-    close(jobs)
-
-    // Collect results of the work done
-    for r := 1; r <= total; r++ {
-        <- results
-    }
+  var logs [total_processors]Log
+  events := EventsCon{}
+  events.createNewConnection()
+  events.wg.Add(total_processors)
+  for i:=0; i<total_processors; i++ {
+    logs[i] = Log{make([]string,0),make([]string,0)}
+    go processJob(&events,&logs[i],i)
+  }
+  events.wg.Wait()
+  events.p.Empty()
+  for i:=0; i<total_processors; i++ {
+    fmt.Println("routine "+strconv.Itoa(i))
+    logs[i].printSuccess()
+    logs[i].printFail()
+  }
 }
